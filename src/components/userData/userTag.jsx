34d85db37e95eb917e16800f1741d1ac4b036a1d
Ashen1919 <2303S0834@saegis.ac.lk>
2024-10-20 16:17:30 +0530

    modify with tailwind css

--- src/components/userData/userTag.jsx
+++ src/components/userData/userTag.jsx
@@ -1,12 +1,12 @@
-import "./userTag.css"
+
 
 function UserTag(props){
     const name = props.name;
     const imageLink = props.imageLink;
     return(
-        <div className="user-data-div">
-            <img src={props.imageLink} alt="user image" />
-            <span>{props.name}</span>
+        <div className="absolute right-0 flex items-center cursor-pointer">
+            <img className="rounded-full w-[75px] h-[69px] " src={props.imageLink} alt="user image" />
+            <span className="text-white ml-[7px] text-xl mr-[7px]">{props.name}</span>
         </div>
     )
 }
