a3a4791ec7e5ae39dbd70e46342570cdbb186a87
Ashen1919 <2303S0834@saegis.ac.lk>
2024-10-21 22:37:05 +0530

    modification

--- src/pages/adminPage/adminPage.jsx
+++ src/pages/adminPage/adminPage.jsx
@@ -2,8 +2,29 @@ import { Link, Route, Routes } from "react-router-dom";
 
 export default function AdminPage(){
     return(
-        <div className="w-full max-h-[100vh] overflow-hidden overflow-y-hidden flex">
-            <div className="w-[20%] h-[100vh] bg-red-800">
+        <div className="w-full max-h-[100vh] overflow-hidden flex">
+            <div className="w-[20%] h-[100vh] bg-red-800 flex flex-col">
+                <div className="text-white text-[20px] hover:text-black ">
+                    <Link to="/admin/bookings">Bookings</Link>
+                </div>
+                <div className="text-white text-[20px]  hover:text-black">
+                    <Link to="/admin/rooms">Rooms</Link>
+                </div>
+                <div className="text-white text-[20px]  hover:text-black">
+                    <Link to="/admin/bookings">Bookings</Link>
+                </div>
+                <div className="text-white text-[20px]  hover:text-black">
+                    <Link to="/admin/bookings">Bookings</Link>
+                </div>
+                <div className="text-white text-[20px]  hover:text-black">
+                    <Link to="/admin/bookings">Bookings</Link>
+                </div>
+                <div className="text-white text-[20px]  hover:text-black">
+                    <Link to="/admin/bookings">Bookings</Link>
+                </div>
+                <div className="text-white text-[20px]  hover:text-black">
+                    <Link to="/admin/bookings">Bookings</Link>
+                </div>
 
             </div>
             <div className="w-[80%] bg-blue-700">
