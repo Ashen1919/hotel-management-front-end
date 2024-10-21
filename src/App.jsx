9d0c673bc13879d68454686c68073c5ba226588c
Ashen1919 <2303S0834@saegis.ac.lk>
2024-10-21 22:21:09 +0530

    modify admin page

--- src/App.jsx
+++ src/App.jsx
@@ -7,13 +7,9 @@ function App() {
   return (
     <BrowserRouter>
       <Routes path = "/*">
-        <Route path = "/" element = {<HomePage/>}></Route>
+        <Route path = "/*" element = {<HomePage/>}></Route>
         <Route path = "/admin/*" element = {<AdminPage/>}/>
-        <Route path="/*" element = {
-          <div className="bg-white w-full h-[600vh]">
-            <h1 className="text-red-500 text-[30px] text-center">404 Not Found!</h1>
-          </div>
-        }/>
+        
       </Routes>
     </BrowserRouter>
   )
--- src/pages/adminPage/adminPage.jsx
+++ src/pages/adminPage/adminPage.jsx
@@ -2,22 +2,13 @@ import { Link, Route, Routes } from "react-router-dom";
 
 export default function AdminPage(){
     return(
-        <div className="w-full h-[100vh] bg-green-800">
-            <h1 className="text-white text-[50px] right-0">Hii Admin</h1>
-            <Link className="mr-8" to={"/admin/rooms"}>Rooms</Link>
-            <Link className="mr-8" to={"/admin/booking"}>Booking</Link>
-            <Routes path="/*">
-                <Route path="/rooms" element = {
-                    <div className="bg-red-600 w-full h-[600vh]">
-                        <h1 className="text-white text-[30px] text-center">Rooms</h1>
-                    </div>
-                }/>
-                <Route path="/booking" element = {
-                    <div className="bg-red-600 w-full h-[600vh]">
-                        <h1 className="text-white text-[30px] text-center">Booking</h1>
-                    </div>
-                }/>
-            </Routes>
+        <div className="w-full max-h-[100vh] overflow-hidden overflow-y-hidden flex">
+            <div className="w-[20%] h-[100vh] bg-red-800">
+
+            </div>
+            <div className="w-[80%] bg-blue-700">
+
+            </div>
         </div>
     )
 }
