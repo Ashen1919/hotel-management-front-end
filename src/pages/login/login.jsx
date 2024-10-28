aa3ef6b22c6928c8cd6ac8045c9a7577b1f2cdb8
Ashen1919 <2303S0834@saegis.ac.lk>
2024-10-28 22:19:22 +0530

    modify code

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -1,40 +1,48 @@
 import { useState } from 'react';
+import axios from 'axios';
 import './login.css';
-const [email, setEmail] = useState("")
-const [password, setPassword] = useState("")
-
-function handleLogin(){
-
-}
 
 export default function LoginPage() {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    function handleLogin() {
+        axios.post("http://localhost:5000/api/users/login", {
+            email: email,
+            password: password
+        }).then((result) => {
+            console.log(result);
+            localStorage.setItem("token", result.data.token)
+            const token = localStorage.getItem("token")
+            console.log(token)
+        }).catch((err) => {
+            console.log(err);
+        });
+    }
+
     return (
         <div className="w-full h-[100vh] back-img flex justify-center items-center">
             <div className="overlay"></div>
             <div className='w-[400px] h-[400px] bg-white bg-opacity-60 backdrop-blur-md rounded-lg flex flex-col justify-center items-center p-4 shadow-md relative'>
                 <h2 className="text-5xl font-bold mb-12">Login</h2>
-                <input 
-                    type="email" 
-                    placeholder="Email" 
+                <input
+                    type="email"
+                    placeholder="Email"
                     className="mb-3 p-2 w-full border border-gray-300 rounded-md"
-                    defaultValue={email}
-                    onChange={(e)=>{
-                        setEmail(e.target.value)
-                    }}
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
                 />
-                <input 
-                    type="password" 
-                    placeholder="Password" 
+                <input
+                    type="password"
+                    placeholder="Password"
                     className="mb-3 p-2 w-full border border-gray-300 rounded-md"
-                    defaultValue={password}
-                    onChange={(e)=>{
-                        setPassword(e.target.value)
-                    }}
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
                 />
                 <button className="p-2 w-full bg-blue-500 text-white rounded-md hover:bg-blue-600" onClick={handleLogin}>
                     Login
                 </button>
             </div>
         </div>
-    )
+    );
 }
