34a477e5004aee66d9d7c897993f9d464261a6fb
Ashen1919 <2303S0834@saegis.ac.lk>
2024-10-29 22:51:50 +0530

    token modify

--- src/components/userData/userTag.jsx
+++ src/components/userData/userTag.jsx
@@ -1,8 +1,12 @@
 
 
 function UserTag(props){
-    const name = props.name;
-    const imageLink = props.imageLink;
+    const token = localStorage.getItem("token")
+
+    if(token != null){
+        console.log(token)
+    }
+
     return(
         <div className="absolute right-0 flex items-center cursor-pointer">
             <img className="rounded-full w-[75px] h-[69px] " src={props.imageLink} alt="user image" />
