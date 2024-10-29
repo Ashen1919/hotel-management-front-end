88e41784f4153c1edccd5d4220c09996b39fcd62
Ashen1919 <2303S0834@saegis.ac.lk>
2024-10-29 22:35:41 +0530

    modify code with env file

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -7,7 +7,7 @@ export default function LoginPage() {
     const [password, setPassword] = useState("");
 
     function handleLogin() {
-        axios.post(import.meta.env.VITE_MONGO_URL+ "/api/users/login" , {
+        axios.post(import.meta.env.VITE_BACKEND_URL+ "/api/users/login" , {
             email: email,
             password: password
         }).then((result) => {
