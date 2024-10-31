bb638562027694ee4ccbc705f8cac0c376360314
Ashen1919 <2303S0834@saegis.ac.lk>
2024-10-31 11:14:54 +0530

    token authorize

--- src/components/userData/userTag.jsx
+++ src/components/userData/userTag.jsx
@@ -1,3 +1,4 @@
+import axios from "axios";
 
 
 function UserTag(props){
@@ -5,6 +6,14 @@ function UserTag(props){
 
     if(token != null){
         console.log(token)
+        axios.get(import.meta.env.VITE_BACKEND_URL+"/api/users/", {
+            headers: {
+                Authorization: "Bearer "+ token,
+                "Content-Type": "application/json",
+            },
+        }).then((res)=>{
+            console.log(res)
+        })
     }
 
     return(
