4592365cb0beef106988872f575c332ccee752e7
Ashen1919 <2303S0834@saegis.ac.lk>
2024-11-01 12:17:27 +0530

    create logout feature

--- src/components/userData/userTag.jsx
+++ src/components/userData/userTag.jsx
@@ -3,9 +3,11 @@ import { useEffect, useState } from "react";
 
 function UserTag(props){
     const[name, setName] = useState("")
-    const token = localStorage.getItem("token")
+    const [userFound, setUserFound] = useState(false)
+    
 
     useEffect(()=>{
+        const token = localStorage.getItem("token")
         if(token != null){
             console.log(token)
             axios.get(import.meta.env.VITE_BACKEND_URL+"/api/users/", {
@@ -16,14 +18,21 @@ function UserTag(props){
             }).then((res)=>{
                 console.log(res)
                 setName(res.data.user.firstName+ " "+ res.data.user.lastName);
+                setUserFound(true);
             })
+        }else{
+            setName("");
         }
-    }, []);
+    }, [userFound]);
 
     return(
         <div className="absolute right-0 flex items-center cursor-pointer">
             <img className="rounded-full w-[75px] h-[69px] " src={props.imageLink} alt="user image" />
             <span className="text-white ml-[7px] text-xl mr-[7px]">{name}</span>
+            <button className="bg-red-600 text-white px-4 py-2 rounded hover:bg-red-700 transition duration-300 mr-4 ml-2" onClick={()=>{
+                localStorage.removeItem("token")
+                setUserFound(false)
+            }}>Logout</button>
         </div>
     )
 }
