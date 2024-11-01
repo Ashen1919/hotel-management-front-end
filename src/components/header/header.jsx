90147f52b264cf992f85159a96d6ab5b1fe5db8f
Ashen1919 <2303S0834@saegis.ac.lk>
2024-11-01 10:27:23 +0530

    accurate the code

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -5,7 +5,7 @@ function Header(){
     return(
         <header className="w-full bg-blue-700 flex h-[72px] relative items-center p-[10px] ">
             <h1 className="text-white text-[30px]">Leonine Villa</h1>
-            <UserTag imageLink = "https://media.istockphoto.com/id/1300845620/vector/user-icon-flat-isolated-on-white-background-user-symbol-vector-illustration.jpg?s=612x612&w=0&k=20&c=yBeyba0hUkh14_jgv1OKqIH0CCSWU_4ckRkAoy2p73o=" name = "Joe Root"/>
+            <UserTag imageLink = "https://media.istockphoto.com/id/1300845620/vector/user-icon-flat-isolated-on-white-background-user-symbol-vector-illustration.jpg?s=612x612&w=0&k=20&c=yBeyba0hUkh14_jgv1OKqIH0CCSWU_4ckRkAoy2p73o=" />
             
 
         </header>
--- src/components/userData/userTag.jsx
+++ src/components/userData/userTag.jsx
@@ -1,25 +1,29 @@
 import axios from "axios";
-
+import { useEffect, useState } from "react";
 
 function UserTag(props){
+    const[name, setName] = useState("")
     const token = localStorage.getItem("token")
 
-    if(token != null){
-        console.log(token)
-        axios.get(import.meta.env.VITE_BACKEND_URL+"/api/users/", {
-            headers: {
-                Authorization: "Bearer "+ token,
-                "Content-Type": "application/json",
-            },
-        }).then((res)=>{
-            console.log(res)
-        })
-    }
+    useEffect(()=>{
+        if(token != null){
+            console.log(token)
+            axios.get(import.meta.env.VITE_BACKEND_URL+"/api/users/", {
+                headers: {
+                    Authorization: "Bearer "+ token,
+                    "Content-Type": "application/json",
+                },
+            }).then((res)=>{
+                console.log(res)
+                setName(res.data.user.firstName+ " "+ res.data.user.lastName);
+            })
+        }
+    }, []);
 
     return(
         <div className="absolute right-0 flex items-center cursor-pointer">
             <img className="rounded-full w-[75px] h-[69px] " src={props.imageLink} alt="user image" />
-            <span className="text-white ml-[7px] text-xl mr-[7px]">{props.name}</span>
+            <span className="text-white ml-[7px] text-xl mr-[7px]">{name}</span>
         </div>
     )
 }
