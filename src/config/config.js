3df66abd30d1c156d6b149ab3da7c97e7e874260
Ashen1919 <2303S0834@saegis.ac.lk>
2024-11-02 22:22:08 +0530

    firebase install

---
+++ src/config/config.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBgPVAC3ls9BX9uSmbzkC7afGpM5wt8ngY",
+  authDomain: "hotel-management-19.firebaseapp.com",
+  projectId: "hotel-management-19",
+  storageBucket: "hotel-management-19.firebasestorage.app",
+  messagingSenderId: "474369662804",
+  appId: "1:474369662804:web:949331dd6dd31ab069f2c1"
+};
+
+const app = initializeApp(firebaseConfig);
