2bc072d1a2fada9325ad24f961a25e926acf07d1
Ashen1919 <2303S0834@saegis.ac.lk>
2024-11-27 01:05:05 +0530

    add a preloader

---
+++ src/components/preLoader/preLoader.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import './preloader.css'; // You can add custom styles for the preloader
+
+const Preloader = () => {
+  return (
+    <div className="preloader-container">
+      <div className="spinner"></div>
+    </div>
+  );
+};
+
+export default Preloader;
--- src/pages/client-page/homePage.jsx
+++ src/pages/client-page/homePage.jsx
@@ -1,14 +1,24 @@
+import React, { useState, useEffect } from "react";
 import Header from "../../components/header/header.jsx";
 import TopBar from "../../components/topBar/topBar.jsx";
+import Preloader from "../../components/preloader/Preloader"; 
 import './homePage.css';
 
 export default function HomePage() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false); 
+    }, 3000); 
+  }, []);
+
   return (
     <>
+      {loading && <Preloader />}
       <div className="w-full h-screen flex flex-col items-center">
-        <TopBar/>
-      <Header />
-        
+        <TopBar />
+        <Header />
       </div>
     </>
   );
