82632bbec830991f1b5a8dd6851a78ba52cfdacd
Ashen1919 <2303S0834@saegis.ac.lk>
2024-11-27 14:10:08 +0530

    update code

--- src/components/userData/userTag.jsx
+++ src/components/userData/userTag.jsx
@@ -1,55 +1,39 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 
-function UserTag(props) {
-  const [name, setName] = useState("");
-  const [userFound, setUserFound] = useState(false);
+function UserTag(props){
+    const[name, setName] = useState("")
+    const [userFound, setUserFound] = useState(false)
+    
 
-  useEffect(() => {
-    const token = localStorage.getItem("token");
-    if (token) {
-      axios
-        .get(import.meta.env.VITE_BACKEND_URL + "/api/users/", {
-          headers: {
-            Authorization: "Bearer " + token,
-            "Content-Type": "application/json",
-          },
-        })
-        .then((res) => {
-          setName(res.data.user.firstName + " " + res.data.user.lastName);
-          setUserFound(true);
-        })
-        .catch((err) => {
-          console.log("Error fetching user data:", err);
-          setUserFound(false);
-        });
-    } else {
-      setName("");
-    }
-  }, []);
+    useEffect(()=>{
+        const token = localStorage.getItem("token")
+        if(token != null){
+            console.log(token)
+            axios.get(import.meta.env.VITE_BACKEND_URL+"/api/users/", {
+                headers: {
+                    Authorization: "Bearer "+ token,
+                    "Content-Type": "application/json",
+                },
+            }).then((res)=>{
+                console.log(res)
+                setName(res.data.user.firstName+ " "+ res.data.user.lastName);
+                setUserFound(true);
+            })
+        }else{
+            setName("");
+        }
+    }, [userFound]);
 
-  return (
-    userFound && (
-      <div className="absolute right-0 flex items-center cursor-pointer">
-        <img
-          className="rounded-full w-[75px] h-[69px]"
-          src={props.imageLink}
-          alt="User"
-        />
-        <span className="text-white ml-[7px] text-xl mr-[7px]">{name}</span>
-        <button
-          className="bg-red-600 text-white px-4 py-2 rounded hover:bg-red-700 transition duration-300 mr-4 ml-2"
-          onClick={() => {
-            localStorage.removeItem("token");
-            setUserFound(false);
-            window.location.reload(); // Reload to reset the logged-in state
-          }}
-        >
-          Logout
-        </button>
-      </div>
+    return(
+        <div className="absolute right-0 flex items-center cursor-pointer">
+            <img className="rounded-full w-[75px] h-[69px] " src={props.imageLink} alt="user image" />
+            <span className="text-white ml-[7px] text-xl mr-[7px]">{name}</span>
+            <button className="bg-red-600 text-white px-4 py-2 rounded hover:bg-red-700 transition duration-300 mr-4 ml-2" onClick={()=>{
+                localStorage.removeItem("token")
+                setUserFound(false)
+            }}>Logout</button>
+        </div>
     )
-  );
 }
-
 export default UserTag;
--- src/pages/signUp/signUp.jsx
+++ src/pages/signUp/signUp.jsx
@@ -65,10 +65,14 @@ export default function SignUpPage() {
     }
 
     if (!image) {
-      toast.error("Please upload a profile picture.");
+      const defaultImage = "https://cloud.appwrite.io/v1/storage/buckets/672a1e700037c646954e/files/674578c80028b7645a44/view?project=672a1dc2000b4396bb7d&project=672a1dc2000b4396bb7d&mode=admin";
+      
+      setImage(defaultImage);
+      
       setIsLoading(false);
       return;
     }
+    
 
     try {
       const response = await storage.createFile(
