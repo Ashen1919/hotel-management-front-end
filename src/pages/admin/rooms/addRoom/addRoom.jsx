0d539be9ec8773eee28e77219dae66525ba3503c
Ashen1919 <2303S0834@saegis.ac.lk>
2024-11-30 01:26:50 +0530

    fix bugs

--- src/pages/admin/rooms/addRoom/addRoom.jsx
+++ src/pages/admin/rooms/addRoom/addRoom.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Client, Storage, ID } from "appwrite";
 import toast from "react-hot-toast";
 import axios from "axios";
