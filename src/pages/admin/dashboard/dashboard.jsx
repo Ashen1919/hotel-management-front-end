1b4794837b7025a9ead157bdbb08454e64c67a11
Ashen1919 <2303S0834@saegis.ac.lk>
2024-12-27 11:29:49 +0530

    modify admin dashboard routing

---
+++ src/pages/admin/dashboard/dashboard.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+
+import {FaDoorOpen} from "react-icons/fa";
+
+export default function Dashboard() {
+  return (
+    <div>
+      {/* Body Section */}
+      <div className="p-5 w-full max-h-[100vh] flex flex-col">
+        <div className="w-full h-auto flex flex-row justify-between gap-4">
+          <div className="w-1/4 p-5 h-[200px] bg-gradient-to-br from-gray-700 to-gray-900 rounded-lg cursor-pointer flex flex-col items-center justify-center shadow-md hover:shadow-lg hover:scale-105 transition-transform duration-300">
+            <div className="flex flex-col items-center">
+              <FaDoorOpen className="text-pink-400 text-6xl mb-2" />
+              <p className="text-6xl font-extrabold text-white mb-1">
+                {roomCount}
+              </p>
+              <p className="text-lg font-medium text-gray-300">Total Rooms</p>
+            </div>
+          </div>
+          <div className="w-1/4 p-5 h-[200px] bg-gradient-to-br from-gray-700 to-gray-900 rounded-lg cursor-pointer flex flex-col items-center justify-center shadow-md hover:shadow-lg hover:scale-105 transition-transform duration-300">
+            <div className="flex flex-col items-center">
+              <FaDoorOpen className="text-pink-400 text-6xl mb-2" />
+              <p className="text-6xl font-extrabold text-white mb-1">14</p>
+              <p className="text-lg font-medium text-gray-300">Rooms</p>
+            </div>
+          </div>
+          <div className="w-1/4 p-5 h-[200px] bg-gradient-to-br from-gray-700 to-gray-900 rounded-lg cursor-pointer flex flex-col items-center justify-center shadow-md hover:shadow-lg hover:scale-105 transition-transform duration-300">
+            <div className="flex flex-col items-center">
+              <FaDoorOpen className="text-pink-400 text-6xl mb-2" />
+              <p className="text-6xl font-extrabold text-white mb-1">14</p>
+              <p className="text-lg font-medium text-gray-300">Rooms</p>
+            </div>
+          </div>
+          <div className="w-1/4 p-5 h-[200px] bg-gradient-to-br from-gray-700 to-gray-900 rounded-lg cursor-pointer flex flex-col items-center justify-center shadow-md hover:shadow-lg hover:scale-105 transition-transform duration-300">
+            <div className="flex flex-col items-center">
+              <FaDoorOpen className="text-pink-400 text-6xl mb-2" />
+              <p className="text-6xl font-extrabold text-white mb-1">14</p>
+              <p className="text-lg font-medium text-gray-300">Rooms</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
