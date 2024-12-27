a3946cc22fa2f94a70b4837b3d7539b86401dacb
Ashen1919 <2303S0834@saegis.ac.lk>
2024-12-27 11:53:19 +0530

    fixing bugs

--- src/pages/admin/dashboard/dashboard.jsx
+++ src/pages/admin/dashboard/dashboard.jsx
@@ -1,8 +1,25 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
-import {FaDoorOpen} from "react-icons/fa";
+import { FaDoorOpen } from "react-icons/fa";
 
 export default function Dashboard() {
+  const [roomCount, setRoomCount] = useState([]);
+  const [roomCountIsLoading, setRoomCountIsLoading] = useState(false);
+
+  useEffect(() => {
+    if (!roomCountIsLoading) {
+      axios
+        .get(import.meta.env.VITE_BACKEND_URL + "/api/rooms/")
+        .then((res) => {
+          setRoomCount(res.data.result.length);
+          setRoomCountIsLoading(true);
+        })
+        .catch((err) => {
+          console.log(err.message);
+        });
+    }
+  }, [roomCountIsLoading]);
+
   return (
     <div>
       {/* Body Section */}
