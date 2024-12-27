45415d7df04489abd0804cca10d6b93204a294a0
Ashen1919 <2303S0834@saegis.ac.lk>
2024-12-27 12:17:51 +0530

    update admin dashboard card counts

--- src/pages/admin/dashboard/dashboard.jsx
+++ src/pages/admin/dashboard/dashboard.jsx
@@ -5,21 +5,54 @@ import { FaDoorOpen } from "react-icons/fa";
 
 export default function Dashboard() {
   const [roomCount, setRoomCount] = useState([]);
-  const [roomCountIsLoading, setRoomCountIsLoading] = useState(false);
+  const [userCount, setUserCount] = useState([]);
+  const [bookingCount, setBookingCount] = useState([]);
+  const [categoriesCount, setCategoriesCount] = useState([]);
+  const [CountIsLoading, setCountIsLoading] = useState(false);
 
   useEffect(() => {
-    if (!roomCountIsLoading) {
+    if (!CountIsLoading) {
       axios
         .get(import.meta.env.VITE_BACKEND_URL + "/api/rooms/")
         .then((res) => {
           setRoomCount(res.data.result.length);
-          setRoomCountIsLoading(true);
+          setCountIsLoading(true);
+        })
+        .catch((err) => {
+          console.log(err.message);
+        });
+
+      axios
+        .get(import.meta.env.VITE_BACKEND_URL + "/api/users/")
+        .then((res) => {
+          setUserCount(res.data.users.length);
+          setCountIsLoading(true);
+        })
+        .catch((err) => {
+          console.log(err.message);
+        });
+
+      axios
+        .get(import.meta.env.VITE_BACKEND_URL + "/api/booking/")
+        .then((res) => {
+          setBookingCount(res.data.List.length);
+          setCountIsLoading(true);
+        })
+        .catch((err) => {
+          console.log(err.message);
+        });
+
+      axios
+        .get(import.meta.env.VITE_BACKEND_URL + "/api/category/")
+        .then((res) => {
+          setCategoriesCount(res.data.categories.length);
+          setCountIsLoading(true);
         })
         .catch((err) => {
           console.log(err.message);
         });
     }
-  }, [roomCountIsLoading]);
+  }, [CountIsLoading]);
 
   return (
     <div>
@@ -38,22 +71,32 @@ export default function Dashboard() {
           <div className="w-1/4 p-5 h-[200px] bg-gradient-to-br from-gray-700 to-gray-900 rounded-lg cursor-pointer flex flex-col items-center justify-center shadow-md hover:shadow-lg hover:scale-105 transition-transform duration-300">
             <div className="flex flex-col items-center">
               <FaDoorOpen className="text-pink-400 text-6xl mb-2" />
-              <p className="text-6xl font-extrabold text-white mb-1">14</p>
-              <p className="text-lg font-medium text-gray-300">Rooms</p>
+              <p className="text-6xl font-extrabold text-white mb-1">
+                {userCount}
+              </p>
+              <p className="text-lg font-medium text-gray-300">Total Users</p>
             </div>
           </div>
           <div className="w-1/4 p-5 h-[200px] bg-gradient-to-br from-gray-700 to-gray-900 rounded-lg cursor-pointer flex flex-col items-center justify-center shadow-md hover:shadow-lg hover:scale-105 transition-transform duration-300">
             <div className="flex flex-col items-center">
               <FaDoorOpen className="text-pink-400 text-6xl mb-2" />
-              <p className="text-6xl font-extrabold text-white mb-1">14</p>
-              <p className="text-lg font-medium text-gray-300">Rooms</p>
+              <p className="text-6xl font-extrabold text-white mb-1">
+                {bookingCount}
+              </p>
+              <p className="text-lg font-medium text-gray-300">
+                Total Bookings
+              </p>
             </div>
           </div>
           <div className="w-1/4 p-5 h-[200px] bg-gradient-to-br from-gray-700 to-gray-900 rounded-lg cursor-pointer flex flex-col items-center justify-center shadow-md hover:shadow-lg hover:scale-105 transition-transform duration-300">
             <div className="flex flex-col items-center">
               <FaDoorOpen className="text-pink-400 text-6xl mb-2" />
-              <p className="text-6xl font-extrabold text-white mb-1">14</p>
-              <p className="text-lg font-medium text-gray-300">Rooms</p>
+              <p className="text-6xl font-extrabold text-white mb-1">
+                {categoriesCount}
+              </p>
+              <p className="text-lg font-medium text-gray-300">
+                Total Categories
+              </p>
             </div>
           </div>
         </div>
